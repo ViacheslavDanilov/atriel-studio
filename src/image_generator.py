1a9d4889b03cb73ebb73e8f3ef1318e2a974ab53
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2024-03-17 18:12:24 +0100

    [Feature]: implement ImageMatcher

--- src/image_generator.py
+++ src/image_generator.py
@@ -6,6 +6,70 @@ from typing import List
 
 import cv2
 import numpy as np
+import pandas as pd
+
+
+class ImageMatcher:
+    """A utility class for matching layout and background images based on their IDs.
+
+    This class provides methods to retrieve file lists from directories, extract IDs from filenames,
+    and create a DataFrame connecting layout and background images with matching IDs.
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_file_list(
+        directory: str,
+        file_template: str,
+    ):
+        file_list = []
+        for root, dirs, files in os.walk(directory):
+            file_list.extend(
+                [os.path.join(root, file) for file in fnmatch.filter(files, file_template)],
+            )
+        file_list.sort()
+        return file_list
+
+    @staticmethod
+    def extract_id(
+        path: str,
+    ):
+        filename = str(Path(path).stem)
+        id = filename.split('_')[1]
+        return id
+
+    def create_dataframe(
+        self,
+        layout_paths: List[str],
+        bg_paths: List[str],
+    ) -> pd.DataFrame:
+
+        file_dict: dict = {
+            'id': [],
+            'layout_path': [],
+            'layout_name': [],
+            'background_path': [],
+            'background_name': [],
+        }
+        for bg_path in bg_paths:
+            id_bg = self.extract_id(path=bg_path)
+            for layout_path in layout_paths:
+                id_layout = self.extract_id(path=layout_path)
+
+                if id_bg == id_layout:
+                    file_dict['id'].append(id_bg)
+                    file_dict['layout_path'].append(layout_path)
+                    file_dict['layout_name'].append(Path(layout_path).name)
+                    file_dict['background_path'].append(bg_path)
+                    file_dict['background_name'].append(Path(bg_path).name)
+                else:
+                    continue
+
+        df = pd.DataFrame(file_dict)
+
+        return df
 
 
 class ImageGenerator:
@@ -198,8 +262,16 @@ class ImageGenerator:
 
 if __name__ == '__main__':
 
-    sample_dir = 'data/input/highlights/01'
-    save_dir = 'data/output/highlights/'
+    sample_dir = 'data/input/stories/marketing_01'
+    save_dir = 'data/output/stories/'
+
+    layout_dir = os.path.join(sample_dir, 'layouts')
+    bg_dir = os.path.join(sample_dir, 'backgrounds')
+
+    matcher = ImageMatcher()
+    layout_paths = matcher.get_file_list(layout_dir, 'layout*.[jpPJ][nNpP][gG]')
+    bg_paths = matcher.get_file_list(bg_dir, 'background*.[jpPJ][nNpP][gG]')
+    df = matcher.create_dataframe(layout_paths, bg_paths)
 
     processor = ImageGenerator(
         num_images=5,

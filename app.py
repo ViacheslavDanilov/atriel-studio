6b777497e539abbf51ceec59a64d46bd4059c3cd
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2024-03-18 00:06:50 +0100

    [Fix]: change pipeline for compatibility with new pipeline

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
+import os
+
 import gradio as gr
 
-from src.image_generator import ImageGenerator
+from src.image_generator import ImageGenerator, ImageMatcher
 
 
 def process_sample(
     sample_dir: str,
     save_dir: str,
-    num_images: int,
+    num_images_per_bg: int,
     scaling_factor: float,
     seed: int,
 ) -> str:
-    processor = ImageGenerator(
-        num_images=num_images,
+
+    # Initialize ImageMatcher and ImageGenerator instances
+    matcher = ImageMatcher()
+    generator = ImageGenerator(
+        num_images_per_bg=num_images_per_bg,
         scaling_factor=scaling_factor,
         seed=seed,
     )
-    processor.process_sample(
+
+    # Get metadata with layout and background pairs
+    layout_dir = os.path.join(sample_dir, 'layouts')
+    bg_dir = os.path.join(sample_dir, 'backgrounds')
+    layout_paths = matcher.get_file_list(layout_dir, 'layout*.[jpPJ][nNpP][gG]')
+    bg_paths = matcher.get_file_list(bg_dir, 'background*.[jpPJ][nNpP][gG]')
+    df = matcher.create_dataframe(layout_paths, bg_paths)
+
+    # Process sample
+    generator.process_sample(
+        df=df,
         sample_dir=sample_dir,
         save_dir=save_dir,
     )
+
     return 'Processing completed'
 
 
@@ -27,7 +43,7 @@ iface = gr.Interface(
     inputs=[
         gr.Textbox(
             label='Sample Directory',
-            value='data/input/highlights/01',
+            value='data/input/highlights/neutral',
             placeholder='Enter path to the sample directory',
         ),
         gr.Textbox(
@@ -38,9 +54,9 @@ iface = gr.Interface(
         gr.Slider(
             minimum=1,
             maximum=20,
-            value=10,
+            value=3,
             step=1,
-            label='Number of Images',
+            label='Number of images for each background',
             info='Choose between 1 and 20',
         ),
         gr.Slider(
@@ -64,9 +80,8 @@ iface = gr.Interface(
     title='Pinterest Image Generation App',
     description='Enter the sample directory, save directory, and parameters to process images.',
     examples=[
-        ['data/input/highlights/01', 'data/output/highlights/', 10, 1.0, 11],
-        ['data/input/stories/04', 'data/output/stories/', 5, 1.0, 11],
-        ['data/input/posts/01', 'data/output/posts/', 10, 1.0, 11],
+        ['data/input/highlights/neutral', 'data/output/highlights/', 10, 1.0, 11],
+        ['data/input/stories/marketing_01', 'data/output/stories/', 5, 1.0, 11],
     ],
 )
 

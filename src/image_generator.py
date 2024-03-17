3735d8fe2d5490bba7eddeefad12c52da6bcd91b
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2024-03-17 19:21:13 +0100

    [WiP]: update ImageGenerator

--- src/image_generator.py
+++ src/image_generator.py
@@ -2,9 +2,10 @@ import fnmatch
 import os
 import random
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 import cv2
+from joblib import Parallel, delayed
 import numpy as np
 import pandas as pd
 
@@ -77,16 +78,16 @@ class ImageGenerator:
 
     def __init__(
         self,
-        num_images: int,
+        num_images_per_bg: int,
         scaling_factor: float,
         seed: int,
     ):
-        self.num_images = num_images
+        self.num_images_per_bg = num_images_per_bg
         self.scaling_factor = scaling_factor
         self.seed = seed
 
     @staticmethod
-    def randomly_select_elements(
+    def _randomly_select_elements(
         lst: List[str],
         num_elements: int,
     ) -> List[str]:
@@ -97,7 +98,7 @@ class ImageGenerator:
         return selected_elements
 
     @staticmethod
-    def uniformly_select_elements(
+    def _uniformly_select_elements(
         lst: List[str],
         num_elements: int,
         shuffle: bool = True,
@@ -127,7 +128,7 @@ class ImageGenerator:
         return selected_elements
 
     @staticmethod
-    def crop_transparent_images(
+    def _crop_transparent_images(
         img: np.ndarray,
     ) -> np.ndarray:
         # Find non-zero alpha values
@@ -144,11 +145,11 @@ class ImageGenerator:
 
         return cropped_img
 
-    def load_layout(
+    def _load_layout(
         self,
         layout_path: str,
     ) -> np.ndarray:
-        layout = cv2.imread(layout_path, cv2.IMREAD_UNCHANGED)
+        layout = cv2.imread(layout_path, cv2.IMREAD_COLOR)
         gray_layout = cv2.cvtColor(layout, cv2.COLOR_BGR2GRAY)
         ret, img_bin = cv2.threshold(gray_layout, 127, 255, cv2.THRESH_BINARY)
         img_bin_resized = cv2.resize(
@@ -161,26 +162,114 @@ class ImageGenerator:
         return img_bin_resized
 
     @staticmethod
-    def load_background(
+    def _load_background(
         img_path: str,
         img_height: int,
         img_width: int,
     ) -> np.ndarray:
         img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-        img_resized = cv2.resize(img, dsize=(img_width, img_height), interpolation=cv2.INTER_CUBIC)
-        return img_resized
+
+        # Check if the dimensions differ
+        if img.shape[0] != img_height or img.shape[1] != img_width:
+            img_resized = cv2.resize(img, dsize=(img_width, img_height), interpolation=cv2.INTER_CUBIC)
+            return img_resized
+        else:
+            return img
 
     @staticmethod
-    def load_image(
+    def _load_image(
         img_path: str,
     ) -> np.ndarray:
-        img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-        if img.shape[2] == 3:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         return img
+
+    def _merge_images(
+            self,
+            img: np.ndarray,
+            img_bg: np.ndarray,
+
+    ):
+        print(img)
+        print(img_bg)
+
+    @staticmethod
+    def _compute_coordinates(
+            img: np.ndarray,
+            img_bg: np.ndarray,
+            centroid: List[float],
+    ) -> Tuple[int, int, int, int]:
+        cx, cy = centroid
+        x_min = int(cx - img.shape[1] / 2)
+        y_min = int(cy - img.shape[0] / 2)
+        x_min = max(x_min, 0)
+        y_min = max(y_min, 0)
+        x_max = min(x_min + img.shape[1], img_bg.shape[1])
+        y_max = min(y_min + img.shape[0], img_bg.shape[0])
+
+        return x_min, y_min, x_max, y_max
+
+
+    def _process_single_background(
+            self,
+            row: pd.Series,
+            sample_dir: str,
+            save_dir: str,
+    ) -> None:
+
+        img_dir = os.path.join(sample_dir, 'images')
+        img_paths = self.get_file_list(img_dir, '*.[jpPJ][nNpP][gG]')
+        img_layout = self._load_layout(row.layout_path)
+        img_bg = self._load_background(
+            row.background_path,
+            img_height=img_layout.shape[0],
+            img_width=img_layout.shape[1],
+        )
+
+        num_objects, _, stats, centroids = cv2.connectedComponentsWithStats(img_layout, connectivity=8)
+
+        for _ in range(self.num_images_per_bg):
+            img_paths_selected = self._randomly_select_elements(img_paths, num_objects - 1)
+            for object_id, img_path in zip(range(1, num_objects), img_paths_selected):
+                img = self._load_image(img_path)
+                # FIXME: chnage crop for 3 channel image
+                img = self._crop_transparent_images(img)
+                img = cv2.resize(img, dsize=(stats[object_id, cv2.CC_STAT_WIDTH], stats[object_id, cv2.CC_STAT_HEIGHT]))
+
+                x_min, y_min, x_max, y_max = self._compute_coordinates(
+                    img=img,
+                    img_bg=img_bg,
+                    centroid=centroids[object_id]
+                )
+
+                if x_max > x_min and y_max > y_min:
+                    # Check if the background has an alpha channel (only for PNG images)
+                    if img_bg.shape[2] == 4:
+                        # Check if the background is completely transparent
+                        if np.max(img_bg[y_min:y_max, x_min:x_max, 3]) == 0:
+                            # Copy non-transparent parts of the foreground directly onto the background
+                            img_bg[y_min:y_max, x_min:x_max] = img[: y_max - y_min, : x_max - x_min]
+                        else:
+                            # Perform alpha blending
+                            alpha_img = img[:, :, 3] / 255.0
+                            alpha_res = 1.0 - alpha_img
+
+                            for c in range(3):
+                                img_bg[y_min:y_max, x_min:x_max, c] = (
+                                        alpha_img * img[: y_max - y_min, : x_max - x_min, c]
+                                        + alpha_res * img_bg[y_min:y_max, x_min:x_max, c]
+                                )
+                    else:
+                        # No alpha channel, treat as 3-channel image
+                        img_bg[y_min:y_max, x_min:x_max] = img[: y_max - y_min, : x_max - x_min]
+
+            save_path = os.path.join(sample_save_dir, f'{sample_name}_{idx + 1:02d}.png')
+            cv2.imwrite(save_path, img_bg, [cv2.IMWRITE_PNG_COMPRESSION, 6])
+
+
 
     def process_sample(
         self,
+        df: pd.DataFrame,
         sample_dir: str,
         save_dir: str,
     ) -> None:
@@ -190,29 +279,31 @@ class ImageGenerator:
         sample_save_dir = os.path.join(save_dir, sample_name)
         os.makedirs(sample_save_dir, exist_ok=True)
 
-        img_dir = os.path.join(sample_dir, 'img')
-        img_paths = self.get_file_list(img_dir, '*.[pP][nN][gG]')
-        layout_path = os.path.join(sample_dir, 'layout.png')
-        img_layout = self.load_layout(layout_path)
-        bg_paths_ = self.get_file_list(sample_dir, 'background*.[pP][nN][gG]')
-        # fmt: off
-        bg_paths = self.uniformly_select_elements(bg_paths_, self.num_images, shuffle=True)
-        num_objects, _, stats, centroids = cv2.connectedComponentsWithStats(img_layout, connectivity=8)
-        # fmt: on
+        # Iterate over layout-background pairs
+        _ = Parallel(n_jobs=1)(     # TODO: set -1
+            delayed(self._process_single_background)(
+                row=row,
+                sample_dir=sample_dir,
+                save_dir=save_dir,
+            )
+            for row in df.itertuples()
+        )
+
+
         for idx, img_back_path in enumerate(bg_paths):
-            img_bg = self.load_background(
+            img_bg = self._load_background(
                 img_back_path,
                 img_height=img_layout.shape[0],
                 img_width=img_layout.shape[1],
             )
 
-            img_paths_selected = self.randomly_select_elements(img_paths, num_objects - 1)
+            img_paths_selected = self._randomly_select_elements(img_paths, num_objects - 1)
 
             for object_id, img_path in zip(range(1, num_objects), img_paths_selected):
                 cx, cy = centroids[object_id]
 
-                img = self.load_image(img_path)
-                img = self.crop_transparent_images(img)
+                img = self._load_image(img_path)
+                img = self._crop_transparent_images(img)
                 img_size = (
                     stats[object_id, cv2.CC_STAT_WIDTH],
                     stats[object_id, cv2.CC_STAT_HEIGHT],
@@ -274,11 +365,12 @@ if __name__ == '__main__':
     df = matcher.create_dataframe(layout_paths, bg_paths)
 
     processor = ImageGenerator(
-        num_images=5,
-        scaling_factor=1,
+        num_images_per_bg=5,
+        scaling_factor=2,
         seed=11,
     )
     processor.process_sample(
+        df=df,
         sample_dir=sample_dir,
         save_dir=save_dir,
     )

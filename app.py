7a5d52ced3e98cbde22b9b4e52e8521ce5fd6db4
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2024-04-25 22:24:05 +0200

    [Fix]: add draft version of tabbed app

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ from src.image_data.image_generator import ImageGenerator
 from src.image_data.image_matcher import ImageMatcher
 
 
-def process_sample(
+def generate_images(
     sample_dir: str,
     save_dir: str,
     num_images_per_bg: int,
     scaling_factor: float,
     seed: int,
 ) -> str:
-
     # Initialize ImageMatcher and ImageGenerator instances
     matcher = ImageMatcher()
     generator = ImageGenerator(
@@ -36,11 +35,31 @@ def process_sample(
         save_dir=save_dir,
     )
 
-    return 'Processing Complete'
+    return 'Images generated successfully!'
 
 
-iface = gr.Interface(
-    fn=process_sample,
+def generate_csv(
+    data_dir: str,
+    save_dir: str,
+    pins_per_day_canva_instagram_templates: int,
+    pins_per_day_instagram_highlight_covers: int,
+    pins_per_day_instagram_puzzle_feed: int,
+    pins_per_day_business_cards: int,
+    pins_per_day_airbnb_welcome_book: int,
+    pins_per_day_price_and_service_guide: int,
+    num_csv_files: int,
+    max_pins_per_csv: int,
+    remove_local_files: bool,
+    start_date: str,
+    seed: int,
+) -> str:
+    # Your processing code here
+
+    return 'CSV(s) generated successfully!'
+
+
+tab1 = gr.Interface(
+    fn=generate_images,
     inputs=[
         gr.Textbox(
             label='Sample Directory',
@@ -78,12 +97,114 @@ iface = gr.Interface(
         ),
     ],
     outputs=gr.Textbox(label='Status'),
-    title='Image & CSV Generation App for Pinterest',
     description='Enter the sample directory, save directory, and parameters to process images.',
     examples=[
         ['data/input/highlights/neutral', 'data/output/highlights/', 10, 1.0, 11],
         ['data/input/stories/marketing_01', 'data/output/stories/', 5, 1.0, 11],
     ],
+)
+
+inputs = [
+    gr.Textbox(
+        label='Data Directory',
+        value='data/csv_generation/',
+        placeholder='Enter the data directory',
+    ),
+    gr.Textbox(
+        label='Save Directory',
+        value='data/csv_generation/',
+        placeholder='Enter the save directory',
+    ),
+    gr.Slider(
+        minimum=0,
+        maximum=20,
+        value=1,
+        step=1,
+        label='Canva Instagram Templates',
+        info='Choose the number of pins per day for Canva Instagram Templates',
+    ),
+    gr.Slider(
+        minimum=0,
+        maximum=20,
+        value=1,
+        step=1,
+        label='Instagram Highlight Covers',
+        info='Choose the number of pins per day for Instagram Highlight Covers',
+    ),
+    gr.Slider(
+        minimum=0,
+        maximum=20,
+        value=1,
+        step=1,
+        label='Instagram Puzzle Feed',
+        info='Choose the number of pins per day for Instagram Puzzle Feed',
+    ),
+    gr.Slider(
+        minimum=0,
+        maximum=20,
+        value=0,
+        step=1,
+        label='Business Cards',
+        info='Choose the number of pins per day for Business Cards',
+    ),
+    gr.Slider(
+        minimum=0,
+        maximum=20,
+        value=0,
+        step=1,
+        label='Airbnb Welcome Book',
+        info='Choose the number of pins per day for Airbnb Welcome Book',
+    ),
+    gr.Slider(
+        minimum=0,
+        maximum=20,
+        value=0,
+        step=1,
+        label='Price and Service Guide',
+        info='Choose the number of pins per day for Price and Service Guide',
+    ),
+    gr.Textbox(
+        label='Number of CSV Files',
+        value='1',
+        placeholder='Enter the number of CSV files',
+    ),
+    gr.Textbox(
+        label='Max Pins Per CSV',
+        value='3',
+        placeholder='Enter the max pins per CSV',
+    ),
+    gr.Checkbox(
+        label='Remove Local Files',
+        value=True,
+    ),
+    gr.Textbox(
+        label='Start Date',
+        value='',
+        placeholder='Enter the start date',
+    ),
+    gr.Textbox(
+        label='Seed',
+        value='11',
+        placeholder='Enter the seed value',
+    ),
+]
+
+tab2 = gr.Interface(
+    fn=generate_csv,
+    inputs=inputs,
+    outputs=gr.Textbox(label='Status'),
+    title='CSV Generation',
+    description='Enter the configuration for CSV generation.',
+    # examples=[
+    #     'data/csv_generation/', 'data/csv_generation/', 1, 1, 1, 0, 0, 0, 1, 3, True, '', 11
+    # ],
+)
+iface = gr.TabbedInterface(
+    interface_list=[tab1, tab2],
+    tab_names=['Image Generation', 'CSV Generation'],
+    title='Image & CSV Generation App for Pinterest',
+    theme='default',
+    analytics_enabled=True,
 )
 
 if __name__ == '__main__':
